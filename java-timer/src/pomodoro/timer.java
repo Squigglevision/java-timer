e9ba89b242141765475e5140f103f0d2dd295178
Squigglevision <127502256+Squigglevision@users.noreply.github.com>
2023-12-03 12:14:38 +1100

    Create repo with file structure

---
+++ java-timer/src/pomodoro/timer.java
@@ -0,0 +1,22 @@
+package pomodoro;
+
+import javax.swing.JFrame;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class timer  extends JFrame implements ActionListener{
+
+    private JButton start;
+    private JButton reset;
+    private JLabel time;
+    private JLabel Title;
+    private int sessionTime = 1200;
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+      
+
+    }
+
+}
